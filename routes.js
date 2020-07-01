0e6e7d2e497138741f88394e1fbe64355d006d9d
Tubaleviao <alvaro.prata.filho@gmail.com>
2020-07-01 02:27:26 +0000

    adding auth for notes and player

--- routes.js
+++ routes.js
@@ -28,8 +28,8 @@ router.get('/logout', functions.logout);
 router.post('/signup', functions.signup);
 router.get('/auth', functions.auth);
 router.get('/', userAuth, functions.dashboard);
-router.get('/player', functions.player); // verified
-router.get('/notes', functions.notes); // verified
+router.get('/player', userAuth, functions.player); // verified
+router.get('/notes', userAuth, functions.notes); // verified
 router.get('/chat', functions.chat);
 router.get('/chat/:room', functions.chat);
 router.get('/shooter', functions.shooter); // verified
