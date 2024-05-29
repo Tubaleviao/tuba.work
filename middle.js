7e2a0daa0da2b87d217b4ed586165f3809867d91
Alvaro <alvaro.prata.filho@gmail.com>
2024-05-29 19:05:33 -0300

    Updating authentication database

--- middle.js
+++ middle.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const mc = require('mongodb').MongoClient;
 let db;
-const url = `${process.env.MONGO_PROTOCOL}://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}${process.env.MONGO_OPTIONS}`;
+const url = `${process.env.MONGO_PROTOCOL}://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_OPTIONS}`;
 const client = new mc(url, { useNewUrlParser: true, useUnifiedTopology: true, });
 const auth = (req, res, next) => {
     try {
